8546e5028299dcd768910cc63a44db125603a74f
Marcos Imfeld <marcosimfeld34@gmail.com>
2023-11-15 15:27:14 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 export default defineConfig({
-  build: {
-    // generate manifest.json in outDir
-    manifest: true,
-    plugins: [react()],
-  },
+  plugins: [react()],
 });
