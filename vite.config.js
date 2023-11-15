5f12d0a5a9ac34cf8913dd4c3882ada2e27c67b5
Marcos Imfeld <marcosimfeld34@gmail.com>
2023-11-15 15:24:50 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     manifest: true,
     rollupOptions: {
       // overwrite default .html entry
-      input: "/path/to/main.js",
+      input: "/src/main.js",
     },
   },
 });
