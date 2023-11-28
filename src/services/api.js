59e16ce1212e677ce6aeb2347d5870369b2bdfbf
Marcos Imfeld <marcosimfeld34@gmail.com>
2023-11-28 12:58:24 -0300

    fix error api url back

--- src/services/api.js
+++ src/services/api.js
@@ -1,2 +1,2 @@
-// export const BACKEND_BASE_URL = "https://church-system-dev-pmhk.4.us-1.fl0.io";
-export const BACKEND_BASE_URL = "http://localhost:3001";
+export const BACKEND_BASE_URL = "https://church-system-dev-pmhk.4.us-1.fl0.io";
+// export const BACKEND_BASE_URL = "http://localhost:3001";
